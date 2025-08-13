890cc51dc46fbbd85835176c958a31a05579c532
Shereef Elias <58827151+shereefelias@users.noreply.github.com>
2025-08-13 09:43:01 -0400

    Create Calculator.java

---
+++ Calculator.java
@@ -0,0 +1,17 @@
+package com.example;
+
+// Calculator class
+
+
+public class Calculator {
+
+    /** Adds two integers. */
+    public int add(int a, int b) {    // add method
+        return a + b;
+    }
+
+    /** Subtracts b from a. */
+    public int subtract(int a, int b) {    // subtract method
+        return a - b;
+    }
+}
